7930a241246300e7cd5267875a4c2226c8b76202
Андрей <141639809+Andrey-Skypro@users.noreply.github.com>
2023-12-22 16:55:55 +0300

    new4

--- Adress.py
+++
@@ -1,7 +0,0 @@
-class Adress :
-    def __init__(self, index, city, street, house, flat):
-        self.index = index
-        self.city = city
-        self.street = street
-        self.house = house
-        self.apartment = flat
--- lesson_3_task_2.py
+++
@@ -1,9 +0,0 @@
-from smartphone import Smartphone
-catalog = []
-catalog.append(Smartphone("Apple", "iPhone X", "+1234567890"))
-catalog.append(Smartphone("Samsung", "Galaxy S10", "+0987654321"))
-catalog.append(Smartphone("Google", "Pixel 5", "+1122334455"))
-catalog.append(Smartphone("OnePlus", "8 Pro", "+2233445566"))
-catalog.append(Smartphone("Xiaomi", "Mi 11", "+3344556677"))
-
-print(f"{"марка_телефона"} - {"модель_телефона"}. {"абонентский_номер"}.")
