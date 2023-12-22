1da54871c311b5b5951732f8b52467ba62aeb6c0
Андрей <141639809+Andrey-Skypro@users.noreply.github.com>
2023-12-22 17:08:24 +0300

    n2

---
+++ homework3/lesson_3_task_1.py
@@ -0,0 +1,5 @@
+from user import User
+user1 = User("alex", "ivanov")
+user2 = User("andrew", "y")
+user3 = User("lara", "kraft")
+my_user = User
---
+++ homework3/user.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, first_name, last_name):
+     print("меня зовут", first_name)
+     print("моя фамилия", last_name)
+     print("меня зовут", "моя фамилия", first_name, last_name)
+        
+
+user1 = User("alex", "ivanov")
+user2 = User("andrew", "y")
+user3 = User("lara", "kraft")
